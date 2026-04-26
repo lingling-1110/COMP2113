4a06e4f409b2d772137d1e184152200db03cd791
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-26 22:32:07 +0800

    Update room.h

--- room.h
+++ room.h
@@ -10,7 +10,12 @@ using namespace std;
 class Room {
 public:
   Room();
-  Room(int t, int l, string n);
+  Room(int t, int lev, string n);
+
+  int getType();
+  string getName();
+  bool getDone();
+  void setDone(bool lev);
 
 private:
   int room_type;
