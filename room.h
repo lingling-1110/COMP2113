50590cbc7e6d50ec4607924918b92032be3cbef5
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-26 22:09:39 +0800

    Update room.h

--- room.h
+++ room.h
@@ -4,3 +4,17 @@
 #include <iostream>
 #include <string>
 #include "player.h"
+
+using namespace std;
+
+class Room {
+public:
+
+private:
+  int room_type;
+  string room_name;
+  int level;
+  bool finished;
+  string key;
+  
+};
