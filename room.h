d2a496199322f3b7b97b8f717eb378272db7bb9a
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 11:51:50 +0800

    Update room.h

--- room.h
+++ room.h
@@ -7,16 +7,22 @@
 using namespace std;
 
 struct Room {
-  int room_id;
+  int room_num;
   string room_name;
   string desc;
   bool locked;
   string key;
   int ans;
-  int location;
-  int dan_path;
+  int key_pos;
+  int trap;
   int hp;
 };
 
+void Room1(Room &r, int diff);
+void Room2(Room &r, int diff);
+void Room3(Room &r, int diff);
+void Room4(Room &r, int diff);
+
+void roomInfo(Room &r);
 
 #endif
