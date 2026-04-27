c520e0e4a4b512aca33fbebc37e90a54941ae30d
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 12:56:55 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
 #include "room.h"
+
+using namespace std;
+
+void def(Room &r) {
+  r.id = 1;
+  r.room_name = "";
+  r.desc = "";
+  r.locked = false;
+  r.key = "";
+  r.ans = INT_MAX;
+  r.dan = 0;
+}
