8621ba827187b22828f7fc4c2406621fa5c9d068
lingling-1110 <u3666575@connect.hku.hk>
2026-04-27 16:15:25 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include <cstdlib>
+#include <ctime> 
+#include "game.h"
+
+using namespace std;
+
+
+Game::Game() {
+  totalRooms = 4;
+  rooms = nullptr;
+  isRunning = true;
+}
+
+Game::~Game() {
+  delete[] rooms;
+}
+
+// to start the game
+void Game::start() {
+
+  cout << "Welcome to Escape the Room!" << endl;
+
+}
