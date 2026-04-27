074e7a9a8aefbb103341388a4a6b884abb21d0b5
yoyotawki-crypto <yoyotawki@gmail.com>
2026-04-27 12:06:23 +0800

    Update player.h

--- player.h
+++ player.h
@@ -1,31 +1,27 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <string>
 #include <vector>
-
+#include <string>
 using namespace std;
 
 class Player {
 public:
     Player();
-    void addItem(string item);
-    bool hasItem(string item);
-    void removeItem(string item);
-    void showInv();
-    bool hasKey();
-    int getHp();
-    void takeDamage(int dmg);
-    void setDiff(int d);  // 0=easy,1=med,2=hard
+    void add(string item);
+    bool has(string item);
+    void remove(string item);
+    void show();
+    void hurt(int dmg);
+    void setDiff(int d);
     int getDiff();
-    int getLevel();
+    int getHp();
     void nextLevel();
+    int getLevel();
 
 private:
     vector<string> inv;
-    int hp;
-    int diff;
-    int level;
+    int hp, diff, level;
 };
 
 #endif
