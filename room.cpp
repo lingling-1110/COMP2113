1e4979d9d0083cb429d7e3fb8ecae8f5695e968d
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 17:03:27 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -14,12 +14,22 @@ void def(Room &r) {
   r.trap = 0;
 }
 
-void create_rm(Room &r, int num, string name, int diff) {
+void create_rm(Room &r, int num, string name, string diff) {
   r.room_num = num;
   r.room_name = name;
   r.locked = false;
-  r.trap = diff * 10;
+  
+  int multi;
+  if (diff == "EASY") {
+    multi = 0;
+  } else if (diff == "MEDIUM") {
+    multi = 1;
+  } else if (diff == "HARD") {
+    multi = 2;
+  }
 
+  r.trap = multi * 10;
+  
   if (num == 2) {
     r.ans = 9999;
   } else {
@@ -28,17 +38,18 @@ void create_rm(Room &r, int num, string name, int diff) {
 }
 
 void clue(Room &r, string diff) {
-  cout << "[It's time for hints!]" << endl;
   if (diff == "EASY") {
+    cout << "[It's time for hints!]" << endl;
     cout << "The password is: " << r.ans << endl;
   } else if (diff == "MEDIUM") {
+    cout << "[It's time for hints!]" << endl;
     cout << "The password starts with 9." << endl;
   } else if (diff == "HARD") {
-    cout << "You can do it!" << endl;
+    cout << "Almost there! You can do it!" << endl;
   }
 }
 
-void enter_rm(Room &r, int diff) {
+void enter_rm(Room &r, string diff) {
   cout << "You are entering " << r.room_name << "." << endl;
 
   if (r.room_num == 1) {
@@ -46,10 +57,12 @@ void enter_rm(Room &r, int diff) {
   } else if (r.room_num == 2) {
     cout << "There's a combination lock in front of you!" << endl;
     clue(r, diff);
-  } else if (r.room_num == 3 && diff != HARD) {
-    cout << "There are traps! Mind your health!" << endl;
-  } else if (r.room_num == 3 && diff == HARD) {
-    cout << "Oh no! There are so many traps! Be careful!" << endl;
+  } else if (r.room_num == 3 && diff == "EASY") {
+    cout << "There are traps! Be careful!" << endl;
+  } else if (r.room_num == 3 && diff == "MEDIUM") {
+    cout << "There are some traps! Watch your steps!" << endl;
+  } else if (r.room_num == 3 && diff == "HARD") {
+    cout << "Danger! There are so many traps! Mind your health!" << endl;
   } else if (r.room_num == 4) {
     cout << "The exit is overthere! Grab the last item and let's go!" << endl;
   }
