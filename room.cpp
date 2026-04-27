17c244490dec3fb9258133e9c89b89ba63d99670
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 16:03:43 +0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -10,7 +10,7 @@ void def(Room &r) {
   r.desc = "";
   r.locked = false;
   r.key = "";
-  r.ans = INT_MAX;
+  r.ans = 0;
   r.trap = 0;
 }
 
@@ -25,34 +25,34 @@ void create_rm(Room &r, int num, string name, int diff) {
   } else {
     r.ans = 0;
   }
-  }
+}
 
-void clue(Room &r, string dif) {
+void clue(Room &r, string diff) {
   cout << "[It's time for hints!]" << endl;
-  if (dif == "EASY") {
+  if (diff == "EASY") {
     cout << "The password is: " << r.ans << endl;
   }
-  else if (dif == "MEDIUM") {
+  else if (diff == "MEDIUM") {
     cout << "The password starts with 9." << endl;
-  else if (dif == "HARD") {
+  else if (diff == "HARD") {
     cout << "You can do it!" << endl;
   }
-  }
+}
 
 void enter_rm(Room &r, int diff) {
-  cout << "You are entering " << r.name << endl;
+  cout << "You are entering " << r.room_name << "." << endl;
 
-  if (r.num == 1) {
+  if (r.room_num == 1) {
     cout << "This room is locked! Find a key to escape!" << endl;
-  } else if (r.num == 2) {
+  } else if (r.room_num == 2) {
     cout << "There's a combination lock in front of you!" << endl;
     clue(r, dif);
-  } else if (r.num == 3) {
+  } else if (r.room_num == 3) {
     cout << "There are traps! Mind your health!" << endl;
     if (dif == HARD) {
       cout << "Oh no! More traps than expected! Be careful!" << endl;
     }
-  } else if (r.num == 4) {
+  } else if (r.room_num == 4) {
     cout << "The exit is overthere! Grab the last item and let's go!" << endl;
   }
 }
