2fc98f279cb6254d808c00f8c0a73f53148afd2b
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 16:23:07 +0800

    Implement save, load, and delete functions for Room

--- save.cpp
+++ save.cpp
@@ -1,4 +1,53 @@
+#include <fstream>
+#include <iostream>
+
 #include "save.h"
-#include "room.h"
 
 using namespace std;
+
+void save(const Room &r, const string &fname) {
+  ofstream fout;
+  fout.open(fname);
+
+  if (fout.fail()) {
+    cout << "The file could not be opened for saving >_<" << endl;
+  } else {
+    fout << r.room_num << endl;
+    fout << r.room_name << endl;
+    fout << r.desc << endl;
+    fout << r.locked << endl;
+    fout << r.key << endl;
+    fout << r.ans << endl;
+    fout << r.trap << endl;
+
+    fout.close();
+  }
+}
+
+bool load(Room &r, const string &fname) {
+  ifstream fin;
+  fin.open(fname);
+
+  if (fin.fail()) {
+    fin >> r.room_num;
+    fin.ignore();
+    getline(fin, r.room_name);
+    getline(fin, r.desc);
+    fin >> r.locked;
+    fin >> r.key;
+    fin >> r.ans;
+    fin >> r.trap;
+
+    fin.close();
+    return true;
+  }
+
+  return false;
+  
+}
+
+void del(const string &fname) {
+  ofstream ofs(fname);
+  ofs.close();
+  cout << "Save file cleared." << endl;
+}
