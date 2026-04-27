dd53a152a3c73820d105ca8e8816f94659a2d9bc
yoyotawki-crypto <yoyotawki@gmail.com>
2026-04-27 12:16:44 +0800

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,1 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
 
+#include <string>
+using namespace std;
+
+int randNum(int min, int max);
+bool chance(int percent);
+string clue();
+string riddle();
+string riddleAns(string q);
+string mathQ();
+int mathA(string q);
+void pause();
+
+#endif
