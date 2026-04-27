e579c5499c747c7af81120ab3c691a30251d637b
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 15:32:41 +0800

    Implement room creation and clue functionality
    Added functions to create a room and provide clues based on difficulty.

--- room.cpp
+++ room.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void def(Room &r) {
-  r.id = 1;
+  r.room_num = 1;
   r.room_name = "";
   r.desc = "";
   r.locked = false;
@@ -13,3 +13,29 @@ void def(Room &r) {
   r.ans = INT_MAX;
   r.trap = 0;
 }
+
+void create_rm(Room &r, int num, string name, int diff) {
+  r.room_num = num;
+  r.room_name = name;
+  r.locked = false;
+  r.trap = diff * 10;
+
+  if (num == 2) {
+    r.ans = 9999;
+  } else {
+    r.ans = 0;
+  }
+  }
+
+void clue(Room &r, string diff) {
+  cout << "[It's time for hints!]" << endl;
+  if (diff == "EASY") {
+    cout << "The password is: " << r.ans << endl;
+  }
+  else if (diff == "MEDIUM") {
+    cout << "The password starts with 9." << endl;
+  else if (diff == "HARD") {
+    cout << "You can do it!" << endl;
+  }
+  }
+
