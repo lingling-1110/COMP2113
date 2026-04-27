03282721ebdd555dd44bc78a41f40eb191ae578d
lingling-1110 <u3666575@connect.hku.hk>
2026-04-27 15:23:44 +0800

    Refactor game object initialization and loop

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+#include <iostream>
 #include "game.h"
+
+using namespace std;
 
 int main() {
     // Create game object
-    Game myGame;
-    myGame.startGame();
+    Game game;
+    game.start();
+    
+    while (!game.isOver()) {
+        game.displayStatus();
+        game.processInput();
+    }
 
+    cout << "Game Over. Thanks for playing!" << endl;
     return 0;
 }
