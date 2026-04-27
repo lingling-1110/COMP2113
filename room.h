40aba2d6ad01c6d16162595d00c33f672a979f53
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-27 10:54:16 +0800

    Update room.h

--- room.h
+++ room.h
@@ -3,37 +3,20 @@
 
 #include <iostream>
 #include <string>
-#include "player.h"
 
 using namespace std;
 
-class Room {
-public:
-
-  // Construct empty room
-  Room();
-  // Define what will be contain in the created room
-  Room(int t, int diff, string n);
-
-  // Get the name of the room
-  string getName();
-  // Get the difficulty
-  int getDiff();
-  // Check if the room is already done
-  bool checkDone();
-  // Set val to be true if the room is already done
-  void setDone(bool val);
-  // Initiate the room's event
-  void start(Player &p);
-  // Provide some clue to the player
-  void clue();
-  
-private:
-  int type;
-  string name;
-  int diff;
-  bool done;
+struct Room {
+  int room_id;
+  string room_name;
+  string desc;
+  bool locked;
   string key;
+  int ans;
+  int location;
+  int dan_path;
+  int hp;
 };
+
 
 #endif
