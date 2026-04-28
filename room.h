3956f43645e8af80e5684c44169322a981c257cd
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-04-28 22:47:24 +0800

    Update room.h

--- room.h
+++ room.h
@@ -7,23 +7,29 @@ using namespace std;
 
 struct Room {
   int id;
-  bool unlocked;
   string room_name;
   string desc;
+  bool unlocked;
   int ans;
   int result;
   int item_pos;
   int item_num;
   int trap;
   int trap_pos;
+  int trapX;
+  int trapY;
+  int w;
+  int h;
+  char layout[6][12];
 };
 
 void def(Room &r);
+bool able(Room &r, int x, int y);
 void create_rm(Room &r, int num, string name, string diff);
-bool inTrap(Room &r, int sel);
+void visual_map(Room &r, int pX, int pY);
+bool inTrap(Room &r, int pX, int pY);
 void discover(Room &r, int sel, bool &found_key);
 void clue(Room &r, string diff);
 void enter_rm(Room &r, string diff, int found_num);
-void visual_map(const Room &r, int px, int py);
 
 #endif
