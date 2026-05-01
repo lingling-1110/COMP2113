58183c9a732365f91d10588d41d8b5739c3f9907
ty-tung <268199660+ty-tung@users.noreply.github.com>
2026-05-01 15:33:33 +0800

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -8,22 +8,29 @@ using namespace std;
 void save(const Room &r, const string &fname) {
   ofstream fout;
   fout.open(fname);
-
+  
   if (fout.fail()) {
     cout << "The file could not be opened for saving >_<" << endl;
   } else {
+    fout << p.gethp() << endl;
+    fout << p.getlevel() << endl;
+    fout << p.getdiff() << endl;
+    
     fout << r.id << endl;
-    fout << r.room_name << endl;
-    fout << r.desc << endl;
     fout << r.unlocked << endl;
     fout << r.ans << endl;
     fout << r.result << endl;
     fout << r.item_pos << endl;
     fout << r.item_num << endl;
     fout << r.trap << endl;
-    fout << r.trap_pos << endl;
+    fout << r.trapX << endl;
+    fout << r.trapY << endl;
+    fout << r.w << endl;
+    fout << r.h << endl;
 
     fout.close();
+
+    cout << "Progress saved to " << fname << endl;
   }
 }
 
@@ -31,28 +38,28 @@ bool load(Room &r, const string &fname) {
   ifstream fin;
   fin.open(fname);
 
-  if (!fin.fail()) {
-    fin >> r.id;
-    fin.ignore();
-    
-    getline(fin, r.room_name);
-    getline(fin, r.desc);
-    
-    fin >> r.unlocked;
-    fin >> r.ans;
-    fin >> r.result;
-    fin >> r.item_pos;
-    fin >> r.item_num;
-    fin >> r.trap;
-    fin >> r.trap_pos;
-    fin.ignore();
-
-    fin.close();
-    return true;
+  if (fin.fail()) {
+    return false;
   }
 
-  return false;
+  int currHp, currLev, currDiff;
+
+  fin >> currHp >> currLev >> currDiff;
   
+  fin >> r.id;
+  fin >> r.unlocked;
+  fin >> r.ans;
+  fin >> r.result;
+  fin >> r.item_pos;
+  fin >> r.item_num;
+  fin >> r.trap;
+  fin >> r.trapX;
+  fin >> r.trapY;
+  fin >> r.w;
+  fin >> r.h;
+
+  fin.close();
+  return true;
 }
 
 void del(const string &fname) {
