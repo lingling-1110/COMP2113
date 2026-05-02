2a2e970b912aa976e6611547530f0b038fb2120c
Ip Hoi Ling <u3666575@connect.hku.hk>
2026-05-02 17:47:48 +0800

    Refactor save and load functions for game state
    Refactored save and load functions to simplify game saving and loading. Introduced saveGame and loadGame functions for better clarity and functionality.

--- save.cpp
+++ save.cpp
@@ -1,110 +1,35 @@
+#include "save.h"
 #include <fstream>
 #include <iostream>
-#include <cstdio>
-
-#include "save.h"
-#include "player.h"
 
 using namespace std;
 
-void save(const Player &p, const Room &r, const string &fname) {
-  ofstream fout;
-  fout.open(fname);
-  
-  if (fout.fail()) {
-    cout << "The file could not be opened for saving >_<" << endl;
-  } else {
-    fout << p.getHp() << endl;
-    fout << p.getLevel() << endl;
-
-    if (p.getDiff() == "EASY") {
-      diffNum = 1;
-    } else if (p.getDiff() == "MEDIUM") {
-      diffNum = 2;
-    } else if (p.getDiff() == "HARD") {
-      diffNum = 3;
+void saveGame(const Player& player) {
+    ofstream file("savegame.txt");
+    if (file.is_open()) {
+        file << player.x << endl;
+        file << player.y << endl;
+        file << player.hp << endl;
+        file << player.currentLevel << endl;
+        file << player.difficulty << endl;
+        file.close();
+        cout << "Game saved!" << endl;
+        cout << "(Press Enter to continue...)" << endl;
+        cin.ignore();
+        cin.get();
     }
-    fout << diffNum << endl;
-    
-    fout << r.id << endl;
-    fout << r.unlocked << endl;
-    fout << r.ans << endl;
-    fout << r.result << endl;
-    fout << r.item_pos << endl;
-    fout << r.item_num << endl;
-    fout << r.trap << endl;
-    fout << r.trapX << endl;
-    fout << r.trapY << endl;
-    fout << r.w << endl;
-    fout << r.h << endl;
-
-    fout.close();
-
-    cout << "Progress saved to " << fname << endl;
-  }
 }
 
-bool load(Player &p, Room &r, const string &fname) {
-  ifstream fin;
-  fin.open(fname);
-
-  if (fin.fail()) {
+bool loadGame(Player& player) {
+    ifstream file("savegame.txt");
+    if (file.is_open()) {
+        file >> player.x;
+        file >> player.y;
+        file >> player.hp;
+        file >> player.currentLevel;
+        file >> player.difficulty;
+        file.close();
+        return true;
+    }
     return false;
-  }
-
-  int currHp, currLev, currDiff;
-
-  fin >> currHp >> currLev >> currDiff;
-
-  p.setHp(currHp);
-  p.setLevel(currLev);
-
-  if (currDiff == 2) {
-    p.setDiff("MEDIUM");
-  } else if (currDiff == 3) {
-    p.setDiff("HARD");
-  } else {
-    p.setDiff("EASY");
-  }
-  
-  fin >> r.id;
-  fin >> r.unlocked;
-  fin >> r.ans;
-  fin >> r.result;
-  fin >> r.item_pos;
-  fin >> r.item_num;
-  fin >> r.trap;
-  fin >> r.trapX;
-  fin >> r.trapY;
-  fin >> r.w;
-  fin >> r.h;
-
-  fin.close();
-
-  free_rm(r);
-  r.layout = new char*[r.h];
-  for (int i = 0; i < r.h; i++) {
-    r.layout[i] = new char[r.w];
-    for (int j = 0; j < r.w; j++) {
-      if (i == 0 || i == r.h - 1 || j == 0 || j == r.w - 1) {
-        r.layout[i][j] = '#';
-      } else {
-        r.layout[i][j] = '.';
-      }
-    }
-  }
-
-  if (r.h > 3 && r.w > 10) {
-    r.layout[3][10] = 'E';
-  }
-  
-  return true;
-}
-
-void del(const string &fname) {
-  if (remove(fname.c_str()) == 0) {
-    cout << "Save file deleted." << endl;
-  } else {
-    cout << "No save file found to delete." << endl;
-  }
 }
