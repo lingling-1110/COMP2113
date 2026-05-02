1c8df6ea69e0918b0a3b54fa1bcf711e1988c880
Ip Hoi Ling <u3666575@connect.hku.hk>
2026-05-02 09:41:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
-#include <iostream>
+
 #include "game.h"
 
-using namespace std;
-
 int main() {
-    // Create game object
-    Game game;
-    game.start();
-    
-    while (!game.isOver()) {
-        game.displayStatus();
-        game.processInput();
-    }
-
-    cout << "Game Over. Thanks for playing!" << endl;
+    startGame();
     return 0;
 }
