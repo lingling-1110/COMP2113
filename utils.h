46ff4dcf2df96d51006b7e07e834a859b941f686
Ip Hoi Ling <u3666575@connect.hku.hk>
2026-05-02 17:51:47 +0800

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,17 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <string>
-using namespace std;
+#include "player.h"
 
-int randNum(int min, int max);
-bool chance(int percent);
-string clue();
-string riddle();
-string riddleAns(string q);
-string mathQ();
-int mathA(string q);      // ADD THIS
-void pause();              // ADD THIS
-void seed();               // ADD THIS (if used elsewhere)
+void handleTrapDamage(int& hp, int difficulty);
+bool solveMathPuzzle(int difficulty);
 
 #endif
